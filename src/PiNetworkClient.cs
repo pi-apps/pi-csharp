4036c9852397e8614efc12dcf917a463105d165f
Dinh Duong Ha <dinhduongha@gmail.com>
2023-02-28 20:12:23 +0700

    Add first code to project

---
+++ src/PiNetworkClient.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using RestSharp;
+using stellar_dotnet_sdk.responses;
+using stellar_dotnet_sdk;
+
+namespace PiNetworkNet
+{
+    public class PiNetworkClient
+    {
+        private readonly RestClient _restClient = new RestClient("https://api.minepi.com/v2");
+        private string _apiKey;
+        public PiNetworkClient(string apiKey)
+        {
+            _restClient.AddDefaultHeader("Content-Type", "application/json");
+            //_restClient.AddDefaultHeader("Accept", "application/json");
+            _apiKey = apiKey;
+        }
+
+        public async Task<PiAuthDto> Me(string accessToken)
+        {
+            var request = new RestRequest("/me");
+            request.AddHeader("Accept", $"application/json");
+            request.AddHeader("Authorization", $"Bearer {accessToken}");
+            var user = await _restClient.GetAsync<PiAuthDto>(request);
+            return user;
+        }
+
+        public async Task<List<PaymentDto>> GetIncompleteServerPayments()
+        {
+            try
+            {
+                var request = new RestRequest("/payments/incomplete_server_payments");
+                request.AddHeader("Accept", $"application/json");
+                request.AddHeader("Authorization", $"Key {_apiKey}");
+                var payments = await _restClient.GetAsync<IncompleteServerPayments>(request);
+                if (payments.IncompletePayments != null && payments.IncompletePayments.Count > 0)
+                    return payments.IncompletePayments;
+            }
+            catch (Exception e)
+            {
+                var str = e.Message;
+            }
+            return null;
+        }
+
+        public async Task<PaymentDto> Create(CreatePaymentDto dto)
+        {
+            try
+            {
+                var request = new RestRequest($"/payments");
+                request.AddHeader("Authorization", $"Key {_apiKey}");
+                request.AddHeader("Accept", $"application/json");
+                var str = JsonConvert.SerializeObject(dto);
+                request.AddJsonBody(JsonConvert.SerializeObject(dto));
+                var response = _restClient.ExecutePost(request);
+                if (response.IsSuccessful)
+                {
+                    var payment = JsonConvert.DeserializeObject<PaymentDto>(response.Content);
+                    if (string.IsNullOrEmpty(payment.ToAddress))
+                    {
+                        return await Get(payment.Identifier);
+                    }
+                    return payment;
+                }
+                else
+                {
+                    try
+                    {
+                        PiNetworkError error = JsonConvert.DeserializeObject<PiNetworkError>(response.Content);
+                        if (error != null)
+                        {
+                            throw new PiNetworkException()
+                            {
+                                PiError = error,
+                            };
+                        }
+                        else
+                        {
+                            throw new Exception($"{response.Content}");
+                        }
+                    }
+                    catch
+                    {
+                        throw;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<PaymentDto> Get(string identifier)
+        {
+            try
+            {
+                var request = new RestRequest($"/payments/{identifier}");
+                request.AddHeader("Authorization", $"Key {_apiKey}");
+                request.AddHeader("Accept", $"application/json");
+                var payment = await _restClient.GetAsync<PaymentDto>(request);
+                return payment;
+            }
+            catch { return null; }
+        }
+
+        public async Task<PaymentDto> Approve(string identifier)
+        {
+            try
+            {
+                var request = new RestRequest($"/payments/{identifier}/approve");
+                request.AddHeader("Authorization", $"Key {_apiKey}");
+                var payment = await _restClient.PostAsync<PaymentDto>(request);
+                return payment;
+            }
+            catch { return null; }
+        }
+
+        public async Task<PaymentDto> Complete(string identifier, string tx)
+        {
+            try
+            {
+                var request = new RestRequest($"/payments/{identifier}/complete");
+                request.AddHeader("Accept", $"application/json");
+                request.AddHeader("Authorization", $"Key {_apiKey}");
+                var txid = new Tx() { TxId = tx };
+                request.AddJsonBody(JsonConvert.SerializeObject(txid));
+                var payment = await _restClient.PostAsync<PaymentDto>(request);
+                return payment;
+            }
+            catch { return null; }
+        }
+
+        public async Task<PaymentDto> Cancel(string identifier)
+        {
+            try
+            {
+                var request = new RestRequest($"/payments/{identifier}/cancel");
+                request.AddHeader("Accept", $"application/json");
+                request.AddHeader("Authorization", $"Key {_apiKey}");
+                var payment = await _restClient.PostAsync<PaymentDto>(request);
+                return payment;
+            }
+            catch { return null; }
+        }
+
+        protected async Task<Server> GetServerAsync(string network)
+        {
+            Server server;
+            if (network == "Pi Network")
+            {
+                server = new Server("https://api.mainnet.minepi.com");
+            }
+            else if (network == "Pi Testnet")
+            {
+                server = new Server("https://api.testnet.minepi.com");
+            }
+            else
+            {
+                server = new Server("https://horizon-testnet.stellar.org");
+            }
+            await Task.CompletedTask;
+            return server;
+        }
+
+        public async Task<double> GetAccountBalance(string network, string account)
+        {
+            //Set network and server
+            Server server = await GetServerAsync(network);
+
+            KeyPair keypair;
+            try
+            {
+                if (account.StartsWith("S"))
+                {
+                    keypair = KeyPair.FromSecretSeed(account);
+                }
+                else
+                {
+                    keypair = KeyPair.FromAccountId(account);
+                }
+            }
+            catch
+            {
+                return 0.0;
+            }
+            //Generate a keypair from the account id.
+
+            //Load the account
+            AccountResponse accountResponse = await server.Accounts.Account(keypair.AccountId);
+
+            //Get the balance
+            Balance[] balances = accountResponse.Balances;
+
+            //Show the balance
+            for (int i = 0; i < balances.Length; i++)
+            {
+                Balance asset = balances[i];
+                Console.WriteLine("Asset Code: " + asset.AssetCode);
+                Console.WriteLine("Asset Amount: " + asset.BalanceString);
+                if (asset.AssetType == "native")
+                {
+                    return double.Parse(asset.BalanceString);
+                }
+            }
+            return 0.0;
+        }
+
+        public async Task<SubmitTransactionResponse> SendNativeAssets(string network, string seed, TransactionData data, uint fee = 100000)
+        {
+            //Source keypair from the secret seed
+            KeyPair sourceKeypair = KeyPair.FromSecretSeed(seed);
+            return await SendNativeAssets(network, sourceKeypair, data, fee);
+        }
+
+        public async Task<SubmitTransactionResponse> SendNativeAssets(string network, KeyPair sourceKeypair, TransactionData data, uint fee = 100000)
+        {
+            //Set network and server
+            Server server = await GetServerAsync(network);
+
+            //Destination keypair from the account id
+            KeyPair destinationKeyPair = KeyPair.FromAccountId(data.ToAddress);
+
+            //Load source account data
+            AccountResponse sourceAccountResponse = await server.Accounts.Account(sourceKeypair.AccountId);
+
+            //Create source account object
+            Account sourceAccount = new Account(sourceKeypair.AccountId, sourceAccountResponse.SequenceNumber);
+
+            //Create asset object with specific amount
+            //You can use native or non native ones.
+            Asset asset = new AssetTypeNative();
+            string amount = $"{data.Amount}";
+            try
+            {
+                //Create payment operation
+                PaymentOperation operation = new PaymentOperation.Builder(destinationKeyPair, asset, amount).SetSourceAccount(sourceAccount.KeyPair).Build();
+
+                var Identifier = string.IsNullOrEmpty(data.Identifier) ? $"" : $"{data.Identifier.Trim()}";
+                MemoText memo = new MemoText(string.IsNullOrEmpty(Identifier) ? $"" : Identifier.Substring(0, Math.Min(Identifier.Length, 28)));
+                //Create transaction and add the payment operation we created
+                Transaction transaction = new TransactionBuilder(sourceAccount)
+                                            .AddOperation(operation)
+                                            .AddMemo(memo)
+                                            .SetFee(fee)
+                                            .Build();
+
+                //Sign Transaction
+                transaction.Sign(sourceKeypair, new Network(network));
+
+                //Try to send the transaction
+                var tx = await server.SubmitTransaction(transaction);
+                return tx;
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Send Transaction Failed");
+                Console.WriteLine("Exception: " + exception.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ src/PiNetworkData.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace PiNetworkNet
+{
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class PiAuthDto
+    {
+        [JsonProperty("accessToken")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("user")]
+        public PiUser User { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class PiUser
+    {
+        [JsonProperty("uid")]
+        public string Uid { get; set; }
+
+        [JsonProperty("credentials")]
+        public Credentials Credentials { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class PiValidTime
+    {
+        [JsonProperty("timestamp")]
+        public long TimeStamp { get; set; }
+        [JsonProperty("iso8601")]
+        public DateTimeOffset Iso8601 { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class Credentials
+    {
+        [JsonProperty("scopes")]
+        public List<string> Scopes { get; set; }
+        [JsonProperty("valid_until")]
+        public PiValidTime ValidTime { get; set; }
+    };
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class PaymentDto
+    {
+        [JsonProperty("identifier")]
+        public string Identifier { get; set; }
+
+        [JsonProperty("user_uid")]
+        public string Useruid { get; set; }
+
+        [JsonProperty("amount")]
+        public double Amount { get; set; }
+
+        [JsonProperty("memo")]
+        public string Memo { get; set; }
+
+        [JsonProperty("metadata")]
+        public Metadata Metadata { get; set; }
+
+        [JsonProperty("from_address")]
+        public string FromAddress { get; set; }
+
+        [JsonProperty("to_address")]
+        public string ToAddress { get; set; }
+
+        [JsonProperty("created_at")]
+        //public string created_at { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [JsonProperty("direction")]
+        public string Direction { get; set; }
+
+        [JsonProperty("network")]
+        public string Network { get; set; }
+
+        [JsonProperty("status")]
+        public Status Status { get; set; }
+
+        [JsonProperty("transaction")]
+        public TransactionStatus Transaction { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class Metadata
+    {
+        [JsonProperty("id")]
+        public Guid? Id { get; set; }
+        [JsonProperty("cat")]
+        public string Category { get; set; }
+        [JsonProperty("data")]
+        public object Data { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class Status
+    {
+        [JsonProperty("developer_approved")]
+        public bool DeveloperApproved { get; set; }
+
+        [JsonProperty("transaction_verified")]
+        public bool TransactionVerified { get; set; }
+
+        [JsonProperty("developer_completed")]
+        public bool DeveloperCompleted { get; set; }
+
+        [JsonProperty("cancelled")]
+        public bool Cancelled { get; set; }
+
+        [JsonProperty("user_cancelled")]
+        public bool UserCancelled { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class TransactionStatus
+    {
+        [JsonProperty("txid")]
+        public string TxId { get; set; }
+
+        [JsonProperty("verified")]
+        public bool Verified { get; set; }
+
+        [JsonProperty("_link")]
+        public string Link { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class Tx
+    {
+        [JsonProperty("txid")]
+        public string TxId { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class IncompleteServerPayments
+    {
+        [JsonProperty("incomplete_server_payments")]
+        public List<PaymentDto> IncompletePayments { get; set; }
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class PaymentArgs
+    {
+        [JsonProperty("amount")]
+        public double Amount;
+        [JsonProperty("memo")]
+        public string Memo;
+        [JsonProperty("metadata")]
+        public object Metadata;
+        [JsonProperty("uid")]
+        public string Uid;
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class CreatePaymentDto
+    {
+        [JsonProperty("payment")]
+        public PaymentArgs Payment;
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class TransactionData
+    {
+        public double Amount;
+        public string Identifier;
+        public string FromAddress;
+        public string ToAddress;
+    }
+
+    [JsonObject(ItemNullValueHandling = NullValueHandling.Ignore)]
+    public sealed class PiNetworkError
+    {
+        [JsonProperty("error")]
+        public string ErrorName { get; set; }
+
+        [JsonProperty("error_message")]
+        public string ErrorMessage { get; set; }
+
+        [JsonProperty("payment")]
+        public PaymentDto Payment { get; set; }
+    }
+
+    public class PiNetworkException: Exception
+    {
+        public PiNetworkError PiError { get; set; }
+    }
+}
